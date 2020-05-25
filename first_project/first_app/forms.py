9bbd2518a9efdf5222062c0a53ed0528298c7795
vineetps <itsvineetpal@gmail.com>
2020-05-25 22:58:33 +0530

    project updated

--- first_project/first_app/forms.py
+++ first_project/first_app/forms.py
@@ -1,13 +1,14 @@
 from django import forms
+from django.contrib.admin.widgets import AdminDateWidget
 import datetime
 from .models import ShareDetails
 
 class Form_Stock_Details(forms.ModelForm):
-    # Company_Name = forms.CharField(required=True)
-    # Date_Bought = forms.DateField(initial=datetime.date.today,required=True)
-    # Share_Price = forms.FloatField(required=True)
-    # Shares_Bought = forms.IntegerField(required=True)
+    company_name = forms.CharField(required=True)
+    date_bought = forms.DateField(initial=datetime.date.today,required=True,widget=AdminDateWidget)
+    share_price = forms.FloatField(required=True)
+    shares_bought = forms.IntegerField(required=True)
 
     class Meta:
         model = ShareDetails
-        fields = "__all__"
+        fields = ['company_name','date_bought','share_price','shares_bought']
